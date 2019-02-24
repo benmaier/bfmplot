bed7b4f4b5f7fb5a99886385613a626531ea14b7
Benjamin Maier <benjaminfrankmaier@gmail.com>
2019-02-24 20:43:54 +0100

    v++

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='bfmplot',
-      version='0.0.5',
+      version='0.0.6',
       description="Snippet collections for plots how I make them.",
       url='https://www.github.com/benmaier/bfmplot',
       author='Benjamin F. Maier',
